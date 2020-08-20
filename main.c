eb2c2c7013551f7eb149f3acd3da6fe4d0618424
浅倉麗子 <asakurareiko@protonmail.ch>
2020-08-20 16:52:55 -0400

    Use psp2dbg for logging

--- main.c
+++ main.c
@@ -19,8 +19,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <psp2kern/appmgr.h>
 #include <psp2kern/kernel/modulemgr.h>
-#include <psp2kern/kernel/sysmem.h>
 
+#include <psp2dbg.h>
 #include <taihen.h>
 
 #define USED __attribute__ ((used))
@@ -30,36 +30,64 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 	if ((x) < 0) { goto fail; }\
 } while (0)
 
-#define HOOK_EXPORT(idx, mod, libnid, funcnid, func)\
-	(hook_id[idx] = taiHookFunctionExportForKernel(\
-		KERNEL_PID, hook_ref+idx, mod, libnid, funcnid, func##_hook))
-
 #define N_HOOK 1
 static SceUID hook_id[N_HOOK];
 static tai_hook_ref_t hook_ref[N_HOOK];
 
+static SceUID hook_export(int idx, char *mod, int libnid, int funcnid, void *func) {
+	SceUID ret = taiHookFunctionExportForKernel(KERNEL_PID, hook_ref+idx, mod, libnid, funcnid, func);
+	if (ret >= 0) {
+		SCE_DBG_LOG_INFO("Hooked %d UID %08X\n", idx, ret);
+		hook_id[idx] = ret;
+	} else {
+		SCE_DBG_LOG_ERROR("Failed to hook %d error %08X\n", idx, ret);
+	}
+	return ret;
+}
+#define HOOK_EXPORT(idx, mod, libnid, funcnid, func)\
+	hook_export(idx, mod, libnid, funcnid, func##_hook)
+
+static int UNHOOK(int idx) {
+	int ret = 0;
+	if (hook_id[idx] >= 0) {
+		ret = taiHookReleaseForKernel(hook_id[idx], hook_ref[idx]);
+		if (ret == 0) {
+			SCE_DBG_LOG_INFO("Unhooked %d UID %08X\n", idx, hook_id[idx]);
+			hook_id[idx] = -1;
+			hook_ref[idx] = -1;
+		} else {
+			SCE_DBG_LOG_ERROR("Failed to unhook %d UID %08X error %08X\n", idx, hook_id[idx], ret);
+		}
+	} else {
+		SCE_DBG_LOG_WARNING("Tried to unhook %d but not hooked\n", idx);
+	}
+	return ret;
+}
+
 static SceInt32 sceAppMgrDrmOpen_hook(const SceAppMgrDrmAddcontParam *pParam) {
-	ksceDebugPrintf("%s%s\n", pParam->mountPoint.data, pParam->dirName.data);
+	SCE_DBG_LOG_INFO("%s%s\n", pParam->mountPoint.data, pParam->dirName.data);
 	return TAI_NEXT(sceAppMgrDrmOpen_hook, hook_ref[0], pParam);
 }
 
 static void startup(void) {
+	SCE_DBG_FILELOG_INIT("ux0:/aciddumper.log");
 	memset(hook_id, 0xFF, sizeof(hook_id));
 	memset(hook_ref, 0xFF, sizeof(hook_ref));
 }
 
 static void cleanup(void) {
-	for (int i = 0; i < N_HOOK; i++) {
-		if (hook_id[i] >= 0) { taiHookReleaseForKernel(hook_id[i], hook_ref[i]); }
-	}
+	for (int i = 0; i < N_HOOK; i++) { UNHOOK(i); }
+	SCE_DBG_FILELOG_TERM();
 }
 
 USED int module_start(UNUSED SceSize args, UNUSED const void *argp) {
 	startup();
 	GLZ(HOOK_EXPORT(0, "SceAppMgr", 0xDCE180F8, 0xEA75D157, sceAppMgrDrmOpen));
+	SCE_DBG_LOG_INFO("module_start success\n");
 	return SCE_KERNEL_START_SUCCESS;
 
 fail:
+	SCE_DBG_LOG_ERROR("module_start failed\n");
 	cleanup();
 	return SCE_KERNEL_START_FAILED;
 }
